90eab85af7dbc1968e42dc171818f129a3a44fb4
Matheus Menezes <mdinato30@gmail.com>
2025-04-28 15:50:11 +0200

    Finished Final Project Day 1

---
+++ Python/Bootcamp/day1/band_name_generator.py
@@ -0,0 +1,13 @@
+#Final Project - Day 1
+print("Welcome to the Band Name Generator.")
+
+#Getting the name of the city and storing in city_name
+city_name = input("What's the name of the city you grew up in? ")
+print(city_name)
+
+#Getting the name of the pet and storing in pet_name
+pet_name = input("What's your pet's name? ")
+print(pet_name)
+
+#Generating the band name with the inputs
+print("Your band name could be " + city_name + " " + pet_name)
--- Python/Bootcamp/day1/tasks_day1.py
+++ Python/Bootcamp/day1/tasks_day1.py
@@ -1,3 +1,4 @@
+#Printing string with multiple texts
 print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
 print("2. Knead the dough for 10 minutes.")
 print("3. Add 3g of Salt.")
@@ -13,3 +14,16 @@ print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n"
       "4. Leave to rise for 2 hours.\n"
       "5. Bake at 200 degrees C for 30 minutes.\n"
       "------------------------------------------------------------")
+
+#Concatenating strings
+print("Hello " + "Matheus")
+print("------------------------------------------------------------")
+
+#Input function
+print ("Hello " + input("What is your name? ") + "!")
+print("------------------------------------------------------------")
+
+#Variable
+username = input("What is your name? ")
+length = len(username)
+print("Your name has " + str(length) + " characters")
