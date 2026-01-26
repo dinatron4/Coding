6088dee6fccad019a12fa548c893e38bb0fbff33
Dinato <matheus.dinato@sap.com>
2026-01-26 15:37:19 +0100

    High Score Tracking system with File

--- Python/Bootcamp/day20_21/scoreboard.py
+++ Python/Bootcamp/day20_21/scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import os
 
 ALIGNMENT = "center"
 FONT = ("Courier", 12, "normal")
@@ -8,7 +9,10 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__(visible=False)
         self.score = 0
-        self.high_score = 0
+        file_path = os.getcwd() + "\\Python\\Bootcamp\\day20_21\\data.txt"
+        with open(file_path) as file:
+            self.high_score = file.read()
+        print(self.high_score)
         self.color("white")
         self.penup()
         self.goto(0, 280)
@@ -23,7 +27,10 @@ class Scoreboard(Turtle):
         self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
-        if self.score > self.high_score:
+        if self.score > int(self.high_score):
+            file_path = os.getcwd() + "\\Python\\Bootcamp\\day20_21\\data.txt"
+            with open(file_path, mode="w") as file:
+                file.write(str(self.score))
             self.high_score = self.score
         self.score = 0
         self.update_scoreboard()
