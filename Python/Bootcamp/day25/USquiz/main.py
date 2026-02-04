68c10b4c790a19169d9f121360847bf0bae9adef
Dinato <matheus.dinato@sap.com>
2026-02-04 14:59:17 +0100

    US states quiz game, interface, state name in the board, and quit

---
+++ Python/Bootcamp/day25/USquiz/main.py
@@ -0,0 +1,48 @@
+import turtle
+import pandas
+import time
+from board_states import BoardStates
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.setup(width=800, height= 800)
+image = "./Python/Bootcamp/day25/USquiz/blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# def get_mouse_click_coor(x, y):
+#     print(x,y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+data = pandas.read_csv("./Python/Bootcamp/day25/USquiz/50_states.csv")
+states = data["state"].to_list()
+x_cord = data["x"].to_list()
+y_cord = data["y"].to_list()
+states_dict = {state: (x, y) for (state, x, y) in zip(states, x_cord, y_cord)}
+# print(our_dict)
+
+board_states = BoardStates()
+guessed_states = []
+
+game_is_on = True
+
+while game_is_on:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Guess the State", prompt="What's another state's name?").title()
+    
+    if answer_state in states and answer_state not in guessed_states:
+        position = states_dict[answer_state]
+        board_states.write_state(state=answer_state, coordinate=position)
+        guessed_states.append(answer_state)
+    elif answer_state == "Quit":
+        game_is_on = False
+    elif answer_state in guessed_states:
+        print("You already guessed this one")
+    else:
+        print("Not a state")
+
+    if len(guessed_states) == len(states):
+        game_is_on = False
+    time.sleep(1)
+
+screen.exitonclick()
--- Python/Bootcamp/day25/Weather/main.py
+++ Python/Bootcamp/day25/Weather/main.py
@@ -12,38 +12,80 @@
 import pandas
 
 # data = pandas.read_csv("./Python/Bootcamp/day25/Weather/weather_data.csv")
+# print(data)
 # print(data["temp"])
 
 # data_dict = data.to_dict()
 # # print(data_dict)
 
-# temp_list = data["temp"].to_list()
+# # temp_list = data["temp"].to_list()
 
-# print(sum(temp_list)/len(temp_list))
-# print(data["temp"].mean())
-# print(data["temp"].max())
+# # print(sum(temp_list)/len(temp_list))
+# # print(data["temp"].mean())
+# # print(data["temp"].max())
 
-# #Selecting columns
-# print(data["condition"])
-# print(data.condition)
+# # #Selecting columns
+# # print(data["condition"])
+# # print(data.condition)
 
-#Selecting rows
-# print(data[data.day == "Monday"])
-# print(data[data.temp == data.temp.max()])
+# #Selecting rows
+# # print(data[data.day == "Monday"])
+# # print(data[data.temp == data.temp.max()])
 
-# monday = data[data.day == "Monday"]
-# monday_temp = monday.temp[0]
-# monday_temp_F = monday_temp*9/5+32
-# print(monday_temp_F)
+# # monday = data[data.day == "Monday"]
+# # monday_temp = monday.temp[0]
+# # monday_temp_F = monday_temp*9/5+32
+# # print(monday_temp_F)
 
-# print(monday.condition)
+# # print(monday.condition)
 
-#Create a datafram from scratch
+# #Create a datafram from scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+
+# data = pandas.DataFrame(data_dict)
+# print(data)
+# data.to_csv("./Python/Bootcamp/day25/Weather/new_data.csv")
+
+# data = pandas.read_csv("./Python/Bootcamp/day25/Weather/centralpark_squirrel.csv")
+# # print(data.columns)
+
+# # print(data["Primary Fur Color"].name)
+
+# fur_color = data["Primary Fur Color"].to_list()
+# # print(fur_color)
+
+# list_of_colors = {}
+
+# for color in fur_color:
+#     if color is not None and color not in list_of_colors:
+#         list_of_colors[color] = 0
+#     if color is not None:
+#         list_of_colors[color] += 1
+
+
+# print(list_of_colors)
+# export_data = pandas.DataFrame(list_of_colors, index=[0,3])
+# export_data.to_csv("./Python/Bootcamp/day25/Weather/squirrel_colors.csv")
+
+
+
+data = pandas.read_csv("./Python/Bootcamp/day25/Weather/centralpark_squirrel.csv")
+
+gray_squirrels = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels = len(data[data["Primary Fur Color"] == "Black"])
+
+print(gray_squirrels)
+print(red_squirrels)
+print(black_squirrels)
+
 data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels, red_squirrels, black_squirrels]
 }
 
-data = pandas.DataFrame(data_dict)
-print(data)
-data.to_csv("./Python/Bootcamp/day25/Weather/new_data.csv")
+df = pandas.DataFrame(data_dict)
+df.to_csv("./Python/Bootcamp/day25/Weather/squirrel_colors.csv")
