c3a57434fad87869828214168a5669bba89eb89e
Matheus Menezes <mdinato30@gmail.com>
2026-02-09 20:04:10 +0100

    Removed useless code

--- Python/Bootcamp/day27/main.py
+++ Python/Bootcamp/day27/main.py
@@ -3,13 +3,13 @@ from tkinter import *
 def button_clicked():
     temp_f = entry.get()
     temp_f = int(temp_f)
-    temp_c = round((temp_f - 32)*5/9,2)
-    output.config(text=temp_c)
+    temp_c = round((temp_f - 32)*5/9,1)
+    output.config(text=f"{temp_c}")
 
 window = Tk()
 window.title("Fahrenheit to Celsius")
-window.minsize(width=300, height=100)
-window.config(padx=55, pady=20)
+window.minsize(width=200,height=100)
+window.config(padx=20, pady=20)
 
 #Fixed labels
 label1 = Label(text="°F", font=("Arial", 12))
@@ -20,9 +20,8 @@ label3 = Label(text="°C", font=("Arial", 12))
 label3.grid(column=2, row=1)
 label3.config(pady=15)
 
-
 #Output Label
-output = Label(font=("Arial", 12))
+output = Label(text="0", font=("Arial", 12))
 output.grid(column=1,row=1)
 
 #Button
@@ -31,10 +30,8 @@ button1 = Button(text="Calculate", command=button_clicked)
 button1.grid(column=1, row=2)
 
 #Entry
-entry = Entry(width=10)
+entry = Entry(width=7, font=("Arial", 12))
+entry.focus()
 entry.grid(column=1, row=0)
-
-
-
 
 window.mainloop()
