ef4e0f65da4efb7cd01aff9900c74eeb68de8e56
Matheus Menezes <mdinato30@gmail.com>
2026-03-03 19:54:58 +0100

    Final project, removing known words

--- Python/Bootcamp/day31/main.py
+++ Python/Bootcamp/day31/main.py
@@ -5,10 +5,15 @@ import pandas
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Arial"
 current_card = {}
-
+to_learn = {}
 #---------------------------------------------------#
-data = pandas.read_csv("Python\Bootcamp\day31\data\italian_words.csv")
-to_learn = data.to_dict(orient="records")
+try:
+    data = pandas.read_csv("Python\Bootcamp\day31\data\words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("Python\Bootcamp\day31\data\italian_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 #--------------- FUNCTIONS -------------------------#
 def next_card():
@@ -24,6 +29,13 @@ def flip_card():
     canvas.itemconfig(card_title, text="Portuguese", fill= "white")
     canvas.itemconfig(card_word, text=current_card["Portuguese"], fill="white")
     canvas.itemconfig(card_image, image=main_lg_bg)
+
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("Python\Bootcamp\day31\data\words_to_learn.csv", index=False)
+    next_card()
 
 
 #---------------------------------------------------#
@@ -50,7 +62,7 @@ wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card
 wrong_button.grid(row=1,column=0)
 
 right_image = PhotoImage(file="./Python/Bootcamp/day31/images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=next_card)
+right_button = Button(image=right_image, highlightthickness=0, command=is_known)
 right_button.grid(row=1,column=1)
 
 
